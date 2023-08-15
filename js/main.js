a8b057791f5a40cac360bd88a103f84e4852392d
web-py-g <61905059+web-py-g@users.noreply.github.com>
2023-08-15 16:51:57 +0300

    Add template

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let services_link = document.getElementById("services");
+let services_submenu = document.querySelector(".services");
+
+services_link.onclick = ()=>{
+  services_submenu.classList.toggle("open")
+}
