9d78b9c2d75312ae8ad89379171cb9b602a5efbc
Gleb <nizametdinov22@gmail.com>
2023-08-19 22:49:13 +0300

    template for cards

--- js/main.js
+++ js/main.js
@@ -1,6 +1,25 @@
 let services_link = document.getElementById("services");
 let services_submenu = document.querySelector(".services");
 
-services_link.onclick = ()=>{
-  services_submenu.classList.toggle("open")
-}
+services_link.onclick = () => {
+  services_submenu.classList.toggle("open");
+};
+
+let contacts_link = document.querySelector(".comands");
+let contacts_cards = document.querySelector(".cards");
+
+contacts_link.onclick = () => {
+  contacts_cards.classList.toggle("open");
+};
+
+let popup = document.getElementById("why");
+let close_popup = popup.querySelector(".popup_close");
+let open_popup = document.querySelector('.why');
+
+open_popup.onclick = () => {
+  popup.style.display = "flex";
+};
+
+close_popup.onclick = () => {
+  popup.style.display = "none";
+};
