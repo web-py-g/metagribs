c04a590a108d97edd87c9b21ae144e893a156d27
web-py-g <61905059+web-py-g@users.noreply.github.com>
2023-08-24 17:10:18 +0300

    Гзвфеу

--- js/main.js
+++ js/main.js
@@ -15,6 +15,7 @@ services_link.onclick = () => {
 
 let contacts_link = document.querySelector(".comands");
 let contacts_cards = document.querySelector(".cards");
+let contacts_cards_clsoe = contacts_cards.querySelector(".popup_close");
 
 contacts_link.onclick = () => {
   contacts_cards.classList.toggle("open");
@@ -26,6 +27,10 @@ contacts_link.onclick = () => {
     services_submenu.classList.toggle("open");
 
   popup.style.display = "none";
+};
+
+contacts_cards_clsoe.onclick = () => {
+  contacts_cards.classList.remove("open");
 };
 
 let popup = document.getElementById("why");
@@ -69,4 +74,30 @@ let page_loader = document.querySelector(".page_loader");
 
 window.onload = () => {
   // page_loader.classList.remove("open");
+};
+
+let contact_list_email = document.querySelector(".contact_list_email");
+let contact_form = document.querySelector(".contacts_form_popup");
+
+contact_list_email.onclick = () => {
+  if (!contact_form.classList.contains("open")) {
+    if (!contact_form.classList.contains("close"))
+      contact_form.classList.add("open");
+  } else {
+    contact_form.classList.add("close");
+    setTimeout(() => {
+      contact_form.classList.remove("close");
+      contact_form.classList.remove("open");
+    }, 1000);
+  }
+};
+
+let copy_email = document.querySelector(".copy_email");
+copy_email.onclick = () => {
+  navigator.clipboard.writeText("example@mail.ru").then(() => {
+    copy_email.innerHTML = "Email скопирован в буфер обмена";
+    setTimeout(() => {
+      copy_email.innerHTML = "example@mail.ru";
+    }, 1500);
+  });
 };
